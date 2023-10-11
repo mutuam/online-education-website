d0ab191e224b2bc2fdf788f4d99c70d8f32bc183
mutuam <lodekib@gmail.com>
2023-10-12 00:08:39 +0300

    <fixed some issues>

--- js/script.js
+++ js/script.js
@@ -1,25 +1,31 @@
 $(document).ready(function(){
     $('.fa-bars').click(function(){
-        $(this).toggleclass('fa-times');
-        $('.navbar').toggleclass('nav-toggle');
+        $(this).toggleClass('fa-times'); // Use toggleClass instead of toggleclass
+        $('.navbar').toggleClass('nav-toggle'); // Use toggleClass instead of toggleclass
     });
-    $(Window).on('scroll load',function(){
-        $('.fa-bars').removeclass('fa-times');
-        $('.navbar').removeclass('nav-toggle');
-        if(  $(Window).scrolltop() > 30){
-            $('.header').addclass('header-active');
-        }else{
-            $('.header').removeclass('header-active');
+
+    $(window).on('scroll load', function(){ // Use "window" with a lowercase "w"
+        $('.fa-bars').removeClass('fa-times'); // Use removeClass instead of removeclass
+        $('.navbar').removeClass('nav-toggle'); // Use removeClass instead of removeclass
+        if ($(window).scrollTop() > 30) { // Use "scrollTop" with a lowercase "s"
+            $('.header').addClass('header-active'); // Use addClass instead of addclass
+        } else {
+            $('.header').removeClass('header-active'); // Use removeClass instead of removeclass
         }
-        $('.section'),each(function(){
-           var top =  $(Window).scrolltop();
-           var id =   $(this).attr('id');
-           var height =   $(this).height();
-           var top =    $(this).offset().top - 200;
-           if(top >= offset  && top < height + offset){
-            $('.navbar ul li a').removeclass('active');
-            $('.navbar').find('{href="#'+ id +'"}').addclass('active');
-           }
+
+        $('.section').each(function(){ // Use "each" with a lowercase "e"
+            var top = $(window).scrollTop(); // Use "scrollTop" with a lowercase "s"
+            var id = $(this).attr('id');
+            var height = $(this).height();
+            var offset = $(this).offset().top - 200; // Fix variable name and calculate offset
+
+            if (top >= offset && top < height + offset) {
+                $('.navbar ul li a').removeClass('active'); // Use removeClass instead of removeclass
+                $('.navbar').find('[href="#' + id + '"]').addClass('active'); // Fix the selector
+            }
         });
     });
 });
+
+
+
